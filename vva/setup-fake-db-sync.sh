a8b7e432a3fe15b3db2955b10b85b6bc95a781bc
jankun4 <81573167+jankun4@users.noreply.github.com>
2023-09-27 14:40:32 +0200

    fix

--- vva/setup-fake-db-sync.sh
+++ vva/setup-fake-db-sync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker run --name fake-db-sync-postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=v8hlDV0yMAHHlIurYupj -e POSTGRES_DB=vva  -p 7432:5432 -d postgres
+docker run --name fake-db-sync-postgres -e POSTGRES_USER=test -e POSTGRES_PASSWORD=test -e POSTGRES_DB=vva  -p 7432:5432 -d postgres
 
 docker cp schema.sql $(docker ps -aqf 'name=fake-db-sync-postgres'):/schema.sql
+
+sleep 5
 
 docker exec -u postgres -it $(docker ps -aqf 'name=fake-db-sync-postgres') psql -U postgres -d vva -f schema.sql
